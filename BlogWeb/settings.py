9bf834f244661dc43d1f6e22f71158956647be02
Gaurav Raj <gksagar260@gmail.com>
2021-07-08 08:25:48 +0530

    backward

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -79,13 +79,9 @@ WSGI_APPLICATION = 'BlogWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'blogconfession',
-        'USER':'root',
-        'PASSWORD':os.environ['PASSWORD'],
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
