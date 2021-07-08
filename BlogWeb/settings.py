d7c293cca7a272efb7efa406f7766962a739d350
Gaurav Raj <gksagar260@gmail.com>
2021-07-08 10:09:18 +0530

    last

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY='django-insecure-4aq5gd3p5c#broo=--z88f)sg1o%xpwsep&ti^u9g9rfsr^-$+'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gaurav-blog-confession.herokuapp.com','127.0.0.1']
 
@@ -85,8 +85,7 @@ DATABASES = {
         
     }
 }
-db_from_env=dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
