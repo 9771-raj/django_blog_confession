aabb69c7900dc67c6e8b4046b3378a09ff8b2d86
Gaurav Raj <gksagar260@gmail.com>
2021-07-08 10:05:30 +0530

    add postgresql

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY='django-insecure-4aq5gd3p5c#broo=--z88f)sg1o%xpwsep&ti^u9g9rfsr^-$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
