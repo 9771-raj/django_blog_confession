47928d31ee48ee4d7f9851d0c9ffc1459c3872e8
Gaurav Raj <gksagar260@gmail.com>
2021-07-08 09:56:48 +0530

    added database

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,7 +85,8 @@ DATABASES = {
         
     }
 }
-
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
